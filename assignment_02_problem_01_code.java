ba6f920fa6c2577b1a47edc37c91027d3a2bdd1f
AliaaMamdouh <128325267+AliaaMamdouh@users.noreply.github.com>
2023-04-29 01:08:30 +0200

    Add files via upload

---
+++ assignment_02_problem_01_code.java
@@ -0,0 +1,15 @@
+//aliaa mamdouh 20216557
+import java.util.Scanner; 
+public class assignment_02_problem_01_code{ 
+	public static void main(String []args){ 
+		Scanner scanner = new Scanner(System.in); 
+	
+		
+	int n= scanner.nextInt();
+ 
+			
+			for(int i=1; i<=n; i++){
+				System.out.println(i);
+			}
+		} 
+	} 
